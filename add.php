5221568ad94d87fa20dbeead964a4e65fb75a6c7
MD AL AMIN <01mdalamin1@gmail.com>
2022-02-11 16:59:04 +0600

    Vue, Nuxt #12

---
+++ add.php
@@ -0,0 +1,10 @@
+<?php
+
+# => https://heroicons.com/
+# yarn add v-click-outside =>
+# Accordion => https://codesandbox.io/s/yvq8z580qv
+# =>
+# =>
+# =>
+# =>
+
---
+++ pages/category.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="flex py-6">
+      <div class="w-1/4 pr-6">
+        <Sidebar />
+      </div>
+      <div class="w-3/4 pr-6">Category</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from '../components/Sidebar.vue'
+export default {
+  components: { Sidebar },
+  name: "Category"
+}
+</script>
+
+<style>
+
+</style>
